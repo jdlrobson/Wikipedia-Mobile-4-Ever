e89d9b31b445b0525ec3265bcbf9c9e4cf771bc1
jrobson <jrobson@wikimedia.org>
2013-05-31 11:40:54 -0700

    Redirect mediawiki.org to mobile

--- chrome/main.js
+++ chrome/main.js
@@ -2,5 +2,7 @@ if ( window.location.href.indexOf( '.m.' ) === -1
 	// don't redirect wikipedia landing page
 	&& window.location.href.indexOf( 'www.wiki' ) === -1 ) {
 	var newUrl = window.location.href.replace( /([^\.]).wiki/, '$1.m.wiki' );
+	// redirect mediawiki.org
+	newUrl = window.location.href.replace( /(www).mediawiki.org/, 'm.mediawiki.org' );
 	window.location.href = newUrl;
 }
