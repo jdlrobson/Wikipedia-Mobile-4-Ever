ee08ae8be67f1e4319043217b8b9ccb92ad75073
jrobson <jrobson@wikimedia.org>
2013-05-31 11:47:00 -0700

    Correct stupidity

--- chrome/main.js
+++ chrome/main.js
@@ -1,10 +1,10 @@
 if ( window.location.href.indexOf( '.m.' ) === -1
 	// don't redirect wikipedia landing page
-	&& window.location.href.indexOf( 'www.wiki' ) === -1 ) {
-	var newUrl = window.location.href.replace( /([^\.]).wiki/, '$1.m.wiki' );
+	&& window.location.href.indexOf( 'www.wikipedia' ) === -1 ) {
+	var newUrl = window.location.href.replace( /([^\.]).wikipedia/, '$1.m.wikipedia' );
 	// redirect mediawiki.org
-	newUrl = window.location.href.replace( /(www).mediawiki.org/, 'm.mediawiki.org' );
+	newUrl = newUrl.replace( /www.mediawiki.org/, 'm.mediawiki.org' );
 	// redirect wikimedia.org
-	newUrl = window.location.href.replace( /([^\.]).wikimedia.org/, '$1.m.wikimedia.org' );
+	newUrl = newUrl.replace( /([^\.]).wikimedia.org/, '$1.m.wikimedia.org' );
 	window.location.href = newUrl;
 }
