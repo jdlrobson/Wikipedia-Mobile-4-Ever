4a49a80f7bc91fbf607095bef3ec47df1397769a
jrobson <jrobson@wikimedia.org>
2013-05-31 11:37:59 -0700

    Don't redirect www.wikipedia.org

--- chrome/main.js
+++ chrome/main.js
@@ -1,4 +1,6 @@
-if ( window.location.href.indexOf( '.m.' ) === -1 ) {
+if ( window.location.href.indexOf( '.m.' ) === -1
+	// don't redirect wikipedia landing page
+	&& window.location.href.indexOf( 'www.wiki' ) === -1 ) {
 	var newUrl = window.location.href.replace( /([^\.]).wiki/, '$1.m.wiki' );
 	window.location.href = newUrl;
 }
